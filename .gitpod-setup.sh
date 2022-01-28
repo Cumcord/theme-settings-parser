e0cf5723f93a585a6d8e8ce572485b064cd1e6d7
yellowsink <cainatkinson98@gmail.com>
2022-01-28 10:14:14 +0000

    make my life easier for putting this thing together

---
+++ .gitpod-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+sudo apt-get install apt-transport-https
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install dart gh
+sudo apt-get upgrade -y
+
+gh auth login
+
+git pull
